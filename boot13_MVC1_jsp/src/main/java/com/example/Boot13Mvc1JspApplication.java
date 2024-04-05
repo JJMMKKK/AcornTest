098234ea0b3ce1e3874462833efd4c8903edb753
Haru <Haru@DESKTOP-L06C1RU>
2024-04-05 18:37:04 +0900

    commit

---
+++ boot13_MVC1_jsp/src/main/java/com/example/Boot13Mvc1JspApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot13Mvc1JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot13Mvc1JspApplication.class, args);
+	}
+
+}
---
+++ boot13_MVC1_jsp/src/main/java/com/example/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+//	@GetMapping("/")//get방식
+	@RequestMapping(value = "/")   //http://localhost:8090/app/
+	public String main() {
+		System.out.println("/ 주소 요청///////");
+		System.out.println("/ 주소 요청222222222222222      ///////");
+		System.out.println("/ 주소 요청3333333333333333      ///////");
+
+		return "main";//뷰리졸버 동작
+	}
+}
