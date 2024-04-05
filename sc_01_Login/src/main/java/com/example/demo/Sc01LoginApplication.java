5adcb79e445ccbf13a3b5db325b438eb85327d93
Haru <Haru@DESKTOP-L06C1RU>
2024-04-05 18:51:32 +0900

    commit

---
+++ sc_01_Login/src/main/java/com/example/demo/Sc01LoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc01LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sc01LoginApplication.class, args);
+	}
+
+}
