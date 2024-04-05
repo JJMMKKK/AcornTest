aba088af5c7b222418641622a147f451aa1033a6
Haru <Haru@DESKTOP-L06C1RU>
2024-04-05 18:48:48 +0900

    commit

--- boot13_MVC1_jsp/src/main/java/com/example/controller/MainController.java
+++ boot13_MVC1_jsp/src/main/java/com/example/controller/MainController.java
@@ -11,8 +11,14 @@ public class MainController {
 	@RequestMapping(value = "/")   //http://localhost:8090/app/
 	public String main() {
 		System.out.println("/ 주소 요청///////");
-		System.out.println("/ 주소 요청222222222222222  1    ///////");
-		System.out.println("/ 주소 요청3333333333333333      ///////");
+		System.out.println("/ 주소 요청///////");
+		System.out.println("/ 주소 요청///////");
+		System.out.println("/ 주소 요청///////");
+		System.out.println("/ 주소 요청///////");
+		System.out.println("/ 주소 요청///////");
+		System.out.println("/ 주소 요청///////");
+		System.out.println("/ 주소 요청///////");
+		System.out.println("/ 주소 요청///////");
 
 		return "main";//뷰리졸버 동작
 	}
